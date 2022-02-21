533e25b425b3023bdd2609554abc4d0b13bfddf7
gabmin <rlarkqals@gmail.com>
2022-02-21 22:18:43 +0900

    [feat] 도넛 차트 구현 완료

--- src/components/DnChart.vue
+++ src/components/DnChart.vue
@@ -1,5 +1,22 @@
 <script>
 import { Doughnut } from "vue-chartjs";
+import data from "../assets/data.json";
+
+const id01 = data.filter((info) => {
+  return info.user_id === "00001";
+});
+const id03 = data.filter((info) => {
+  return info.user_id === "00003";
+});
+const id04 = data.filter((info) => {
+  return info.user_id === "00004";
+});
+const id05 = data.filter((info) => {
+  return info.user_id === "00005";
+});
+const id06 = data.filter((info) => {
+  return info.user_id === "00006";
+});
 
 export default {
   extends: Doughnut,
@@ -15,7 +32,13 @@ export default {
             "#FAC859",
             "#5D7AD8",
           ],
-          data: [1, 4, 2, 3, 5],
+          data: [
+            id01.length,
+            id03.length,
+            id04.length,
+            id05.length,
+            id06.length,
+          ],
         },
       ],
       options: {},
